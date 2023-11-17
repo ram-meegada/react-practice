4f11f9393d1c56c90639df0404312663875c1415
ramu-meegada <ramumeegada1811@gmail.com>
2023-11-17 18:04:56 +0530

    first-commit

---
+++ src/pages/login/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+const Login = () => {
+    return(
+        <>
+        <h1>this is Login page</h1>
+        <input></input>
+        </>
+    
+    )
+}
+
+export default Login;

51827e6137cd33c010837eca2a102232642468bf
ram_meegada <ramumeegada1811@gmail.com>
2023-11-27 22:51:16 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './pages/login/login';
 
+
 function App() {
   return (
   <Login/>
--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import Axios from "axios"
+import { useEffect, useState } from 'react'
+// fetch("https://catfact.ninja/fact")
+// .then((res) => res.json())
+// .then((data) => {
+//     console.log(data);
+// });
+
 const Login = () => {
+    const [current, setCurrent] = useState("");
+    function fetchRandomNumber() {
+        Axios("http://127.0.0.1:8000/testing/")
+        .then((res) => {
+            setCurrent(res.data.data);
+        })
+        .catch(error => {
+            console.error('AxiosError:', error);
+          });    
+    }
+    // useEffect(() => {
+    //     fetchRandomNumber();
+    // }, []);
     return(
-        <>
-        <h1>this is Login page</h1>
-        <input></input>
-        </>
-    
+        <div>
+        <h1>this is api fetch response page</h1>
+        <button onClick={fetchRandomNumber}>generate response</button>
+        <p>{current}</p>
+        </div>
     )
 }
 
